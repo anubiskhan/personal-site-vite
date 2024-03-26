9cd305bb326374e7b13e95731141939778f51d02
Kelly Schroeder <kellyrschroeder@gmail.com>
2024-03-25 21:40:40 -0700

    Adds router functionality. Adds files for Me and Things pages

---
+++ src/components/Things.tsx
@@ -0,0 +1,12 @@
+// File: ./components/Things.tsx
+
+const Things = () => {
+    return (
+        <div>
+            <h1>This is the Things page</h1>            
+            <p>Here you can add content for the Things page.</p>
+        </div>
+    );
+}
+
+export default Things;
