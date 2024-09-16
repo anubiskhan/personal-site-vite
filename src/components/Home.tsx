7996c3e913e2365e8b9452792885259631661564
Kelly Schroeder <kellyrschroeder@gmail.com>
2024-09-16 16:17:04 -0700

    Adds Home template

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+
+// File: ./components/Home.tsx
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is the Home page</h1>
+            <p>Here you can add content for the Home page.</p>
+        </div>
+    );
+}
+
+export default Home;
