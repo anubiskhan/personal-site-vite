96b85fdccaa5a2afa823b9c02f1eb20af584d973
Kelly Schroeder <kellyrschroeder@gmail.com>
2024-09-16 16:16:42 -0700

    Adds nav and home links to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import fav from '/fav2.png'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import fav from '/fav2.png'
 import Things from "./components/Things"; // Make sure to create this component
 import Me from "./components/Me"; // Make sure to create this component
+import Home from "./components/Home"; // Make sure to create this component
 
 
 export default function App() {
   return (
     <Router>
-      <Link className="iconGridItem" to="/me">Me</Link>
-      <Link className="iconGridItem" to="/things">Things</Link>
-      <div className="homeContainer">
-        <div className="sideContainer">
-          <div className="sideInnerTop">
-            <h1>Kelly Schroeder</h1>
-            <img src={fav} alt="" />
-          </div>
-        <Routes>
-          <Route path="/things" element={<Things />} />
-          <Route path="/me" element={<Me />} />
-        </Routes>
-
+      <div className="topNav">
+        <div>
+        <Link to="/"><img src={fav} alt="" /></Link>        
+        <h1>Kelly Schroeder</h1>
         </div>
-
+      <div>
+          <Link className="iconGridItem" to="/me">Me</Link>
+          <Link className="iconGridItem" to="/things">Things</Link>
       </div>
+      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/me" element={<Me />} />
+          <Route path="/things" element={<Things />} />
+        </Routes>
     </Router>
   );
 }
